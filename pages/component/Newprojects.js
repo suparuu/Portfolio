807ce6d5bed393fd58f8e3bc81c12ab2a842ce11
suparuu <kkwoo97@naver.com>
2023-04-14 02:51:39 +0900

    need to add animation when i click a button

--- pages/component/Newprojects.js
+++ pages/component/Newprojects.js
@@ -15,12 +15,17 @@ export const Newprojects = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
   const [imgstate, setImgstate] = useState(); //내가 만든 프로젝트 정보 담은 state
   const [swiperIndex, setSwiperIndex] = useState(0);//초기값 0 으로 설정 , swiper index 담는 state
+  const [animation , setAnimation] = useState(false);//초기값 false , 버튼 눌렀을 때 true
 
-
-  //
-
+  const handleExternalLink = (url) => {
+    window.location.href = url;
+  };//내 사이트로 보내주는 함수
   function getIndex(index) {
     setSwiperIndex(index)
+    setAnimation(true);
+    setTimeout(()=>{
+      setAnimation(false);
+    },500)
   }//swiper index값을 state에 담는 함수
   useEffect(() => {
     let datas = [
@@ -29,13 +34,6 @@ export const Newprojects = () => {
         detail: '처음으로 하는 프로젝트 이기도 하고, 클론코딩을 부담없이 할 수 있을거라 생각하여 제주맥주 사이트를 클론코딩을 하게 되었습니다.',
         skills: "HTML5, CSS3, SCSS, JQuery 를 사용 GitHub를 이용하여 사이트 배포",
         site:'https://suparuu.github.io/Project_jejubeer/'
-      },
-      {
-        name: '담배추천사이트', url: '/ciga.png', id: 1,
-        detail: '팀원 들과 협의를 해서 공통적으로 좋아하는 것이 담배 였는데, 비 흡연자도 사이트를 한번 쯤은 방문하게 끔 만들자 라고 생각해서 사이트를 만들게 되었습니다.',
-        skills: "HTML, CSS, SCSS, Javascript, JSON , JQuery를 사용 GitHub를 이용하여 사이트 배포",
-        site:'https://suparuu.github.io/Project_Ciga/'
-
       },
       {
         name: '롤API', url: '/project_lol1.png', id: 2,
@@ -50,14 +48,29 @@ export const Newprojects = () => {
         skills: 'React, Next.js, mySQL, Spotify API 사용 CloudType을 이용하여 사이트 배포 <내용 추가 예정',
         site:'https://suparuu.github.io/Project_jejubeer/'
 
-      }
+      },
+      {
+        name: '담배추천사이트', url: '/ciga.png', id: 1,
+        detail: '팀원 들과 협의를 해서 공통적으로 좋아하는 것이 담배 였는데, 비 흡연자도 사이트를 한번 쯤은 방문하게 끔 만들자 라고 생각해서 사이트를 만들게 되었습니다.',
+        skills: "HTML, CSS, SCSS, Javascript, JSON , JQuery를 사용 GitHub를 이용하여 사이트 배포",
+        site:'https://suparuu.github.io/Project_Ciga/'
+
+      },
     ]
     setImgstate(datas)
   }, [])//프로젝트 이미지, 내용, 이름 수정하기 및 기능 덜 했음 
+  function handleClick(){
+    setAnimation(true);
+    setTimeout(()=>{
+      setAnimation(false);
+    },500)
+  }
+
 
   return (
     <div className={Newprojectscss.sectionBox}>
       <div className={Newprojectscss.leftBox}>
+        {/* <div className={Newprojectscss.textBox}> */}
         <div className={Newprojectscss.textBox}>
           {imgstate && imgstate.map((obj, index) => {
             if(index == swiperIndex){
@@ -66,7 +79,7 @@ export const Newprojects = () => {
                   <p className={Newprojectscss.pjName}>{obj.name}</p>
                   <p className={Newprojectscss.pjDetail}>{obj.detail}</p>
                   <p className={Newprojectscss.pjSkill}>{obj.skills}</p>
-                  <div className={Newprojectscss.gotoProject} onClick={obj.site}>자세히 보기</div>
+                  <div className={Newprojectscss.gotoProject} onClick={()=>{handleExternalLink(obj.site)}}>자세히 보기</div>
                 </>
               )
             }
